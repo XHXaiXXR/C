974c839b120864bdcd534643994a881a251e209f
XHXaiXXR <893675475@qq.com>
2016-12-25 09:49:58 +0800

    2016_12_25_1

---
+++ 项目/大数运算/大数运算（3）——四则运算之减法/BigData.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <string>
+
+typedef long long INT64;
+#define UN_INT64 0xCCCCCCCCCCCCCCCC
+#define MAX_INT64 0x7FFFFFFFFFFFFFFF
+#define MIN_INT64 0x8000000000000000
+
+class BigData
+{
+public:
+	BigData(const INT64 value);
+	BigData(const char* str);
+	BigData operator+(const BigData& bigData);
+	BigData operator-(const BigData& bigData);
+	std::string Add(std::string left,std::string right);
+	std::string Sub(std::string left,std::string right);
+	friend std::ostream& operator<<(std::ostream& _cout,const BigData& bigData);
+
+private:
+	bool _IsINT64OverFlow()const;
+	int _Compare(std::string left,std::string right);
+private:
+	INT64 _value;
+	std::string _strData;
+};
