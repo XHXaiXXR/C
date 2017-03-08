423c7c413abd43f3876b9b3f8562015022c6d34a
XHXaiXXR <893675475@qq.com>
2017-03-08 17:47:55 +0800

    2017_3_8_1

---
+++ Linux/tcp/client.c
@@ -0,0 +1,67 @@
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<unistd.h>
+#include<netinet/in.h>
+#include<stdlib.h>
+#include <arpa/inet.h>
+
+//client...
+
+int main(int argc,char* argv[])
+{
+	if(argc != 3)
+	{
+		printf("Usuge: %s [IP_Addr] [Post]\n",argv[0]);
+		return 0;
+	}
+	//(1)创建套接字
+	int sock = socket(AF_INET, SOCK_STREAM, 0);
+	if(sock < 0)
+	{
+		perror("socket");
+		return 1;
+	}
+	
+	//(2)链接
+	struct sockaddr_in server;
+	server.sin_family = AF_INET;
+	server.sin_port = htons(atoi(argv[2]));
+	server.sin_addr.s_addr = inet_addr(argv[1]);
+
+	//connect是以阻塞的方式链接的,出错返回-1
+	if( connect(sock, (struct sockaddr*)&server, sizeof(server)) < 0 )
+	{
+		//链接失败
+		perror("connect");
+		return 2;
+	}
+
+	//链接成功
+	while(1)
+	{
+		char buf[1024] = {0};
+		printf("Please Enter:");
+		fflush(stdout);
+		ssize_t _s = read(0, buf, sizeof(buf)-1);
+		if(_s > 0)
+		{
+			buf[_s-1] = '\0';
+			write(sock, buf, sizeof(buf)-1);
+		}
+	}
+	colse(sock);
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Linux/tcp/server.c
@@ -0,0 +1,110 @@
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<unistd.h>
+#include<netinet/in.h>
+#include<stdlib.h>
+#include <arpa/inet.h>
+
+//Server...
+
+//启动，返回值是一个监听套接字
+int Startup(const char* addr,const char* port)
+{
+	//(1)创建套接字
+	//socket(当前域(默认是AF_INET),流式套接(默认SOCKET_STREAM),(默认为0)),出错返回-1，否则返回新的文件描述符（默认从3开始）	
+	int listen_sock = socket(AF_INET, SOCK_STREAM, 0);
+	
+	if( listen_sock < 0)
+	{
+		//创建失败
+		perror("socket");
+		exit(1);//直接错误退出
+	}
+	
+	//（2）绑定	
+	struct sockaddr_in local;
+	local.sin_family = AF_INET;//协议家族
+	local.sin_port = htons(atoi(port));//端口号
+	local.sin_addr.s_addr = inet_addr(addr);//IP地址
+	
+	//bind(文件描述符,socket的抽象数据类型的结构体,结构体的长度)，出错返回返回-1
+	if( bind(listen_sock, (struct sockaddr* )&local, sizeof(local)) < 0 )
+	{
+		//绑定失败
+		perror("bind");
+		exit(2);
+	}
+
+	//（3）监听
+	//listen（文件描述符，长度一般为5-8），出错返回-1
+	if( listen( listen_sock, 5) < 0)
+	{
+		//监听失败
+		perror("listen");
+		exit(3);
+	}
+
+	return listen_sock;
+}
+
+int main(int argc,char* argv[])
+{
+	if(argc != 3)
+	{
+		printf("%s [IP_Addr] [Port]\n",argv[0]);
+		return 0;
+	}
+	int listen_sock = Startup(argv[1], argv[2]);	
+	printf("listen_sock = %d\n",listen_sock);
+	
+	//对端套接字
+	struct sockaddr_in remote;
+	int len = sizeof(struct sockaddr);
+	
+	//（4）监听（接收）新的链接请求，用于数据通信
+	while(1)
+	{
+		//建立新的链接
+		int fd = accept(listen_sock, (struct sockaddr*)&remote, &len);
+		if(fd < 0)
+		{
+			perror("accept");
+			continue;
+		}
+
+		while(1)
+		{
+			char buf[1024] = {0};
+			ssize_t _s = read(fd, buf, sizeof(buf)-1);
+			if(_s > 0)
+			{
+				printf("client#	%s\n",buf);
+			}
+			else if(_s == 0)
+			{
+				printf("client print end...\n");
+				break;
+			}
+			else
+			{
+				perror("read");
+			}
+		}
+		close(fd);
+	}
+		
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
